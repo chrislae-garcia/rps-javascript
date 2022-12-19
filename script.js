d52e7546345fe36e4e31ce26ced558080b687c5c
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-12-19 15:38:50 +0800

    Add function playRound()

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function getComputerChoice() {
   }
 }
 
+// Play a round between player and computer
+function playRound(playerSelection, computerSelection) {
+  let playerSelect = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1).toLowerCase();
+  let computerSelect = computerSelection;
+  console.log(`You: ${playerSelect} \nComputer: ${computerSelect}`)
+
+  if (playerSelect == computerSelect) {
+    console.log('Draw!');
+  } else if ((playerSelect == 'Rock' && computerSelect == 'Paper') ||
+    (playerSelect == 'Paper' && computerSelect == 'Scissors') ||
+    (playerSelect == 'Scissors' && computerSelect == 'Rock')) {
+    console.log(`You lose! ${computerSelect} beats ${playerSelect}`);
+  } else {
+    console.log(`You win! ${playerSelect} beats ${computerSelect}`);
+  }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+
+playRound(playerSelection, computerSelection);
